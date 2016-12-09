cde9a4bf6e7e763aafbb82b19ef4bdeb9dd7c1bc
Scott Dixon <dixon.scottr@gmail.com>
2016-12-08 22:14:53 -0500

    Add path to twitter api

--- twitterwarz/urls.py
+++ twitterwarz/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from . import views
+from views import OurTwitterApi
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^twitter_user/(.+)$', OurTwitterApi()),
 ]
